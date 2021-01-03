eec9cc991ac206998c266c15546e182301b37d19
trickerer <onlysuffering@gmail.com>
2021-01-03 08:54:26 +0700

    Init

---
+++ world_spell_linked_spell2020.sql
@@ -0,0 +1,8 @@
+REPLACE INTO `spell_linked_spell`
+(`spell_trigger`,`spell_effect`,`type`,`comment`)
+VALUES
+('-455001','-455101','0','truma: eye - blindness'),
+('-455002','-455102','0','truma: bone - pain'),
+('-455003','-455103','0','truma: heart - bleed'),
+('-455004','-455104','0','truma: brain - unconsciousness'),
+('-455005','-455105','0','truma: st - fraility');
